a99b11e300f84977bcfc3057d64ff2721448287b
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-07 10:41:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * 
+ * @Chad
  */
 public class Main {
 
   /**
    * The method that is executed when you hit the run button.
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner for user input
+    Scanner input = new Scanner(System.in);
+    // ASKS USER FOR NUMBER to start at
+    System.out.println("What number do you want to begin counting from?");
+    int startNum = input.nextInt();
+    // asks user for number to end at
+    System.out.println("What number do you want to end counting at?");
+    int endNum = input.nextInt();
+    int number;
+    if (startNum % 2 == 1) {
+     number = startNum + 1;
+    }else{
+     number = startNum;
+    } 
+    for(int count = number - 2; count < endNum; count+=2){
+      System.out.println(number);
+      number = number + 2;
+    }
   }
 }
